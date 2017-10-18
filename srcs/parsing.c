98c0759f7bbda2814f073f6e24d9435d223ab0ce
Olivier FRANCO <ofranco@e3r4p1.42.fr>
2017-10-18 21:42:18 +0200

    ok

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,11 +6,12 @@
 /*   By: ofranco <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/06 11:14:02 by ofranco           #+#    #+#             */
-/*   Updated: 2017/10/14 20:18:43 by ofranco          ###   ########.fr       */
+/*   Updated: 2017/10/18 21:42:05 by ofranco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
+#include <stdio.h>
 
 int		ft_count(char **tab)
 {
@@ -28,8 +29,15 @@ void	*free_tab(char **tmp)
 
 	k = -1;
 	while (tmp[++k])
-		free(tmp[k]);
-	free(tmp);
+	{
+		if (tmp[k] != NULL)
+		{
+			free(tmp[k]);
+			tmp[k] = NULL;
+		}
+	}
+	if (tmp != NULL)
+		free(tmp);
 	return (NULL);
 }
 
@@ -43,25 +51,25 @@ int		**parse_in_int(char **tab, int line_value)
 	i = 0;
 	if ((grid = (int **)malloc(sizeof(int*) * line_value)) == NULL)
 		return (NULL);
+	grid[line_value] = 0;
 	while (i < line_value)
 	{
 		tmp = ft_split(tab[i], " ");
 		j = 0;
-		if ((grid[i] = (int*)malloc(sizeof(int) * ft_count(tmp) + 1)) == NULL)
+		if ((grid[i] = (int*)malloc(sizeof(int) * ft_count(tmp))) == NULL)
 			return (NULL);
 		while (j < ft_count(tmp))
 		{
 			grid[i][j] = ft_getnbr(tmp[j]);
 			j++;
 		}
-		grid[i][j] = -42;
 		tmp = free_tab(tmp);
 		i++;
 	}
 	return (grid);
 }
 
-char	**fill_tab(int line_value, char *file)
+/*char	**fill_tab(int line_value, char *file)
 {
 	int		fd;
 	int		i;
@@ -74,32 +82,70 @@ char	**fill_tab(int line_value, char *file)
 	while (i < line_value)
 	{
 		get_next_line(fd, &tab[i]);
+		if (check_char(tab[i]) == 0)
+			exit(0);
 		i++;
 	}
 	return (tab);
+}*/
+
+char	**create_map(int y)
+{
+	char **map;
+
+	if ((map = (char **)malloc(sizeof(char*) * y)) == NULL)
+		return (NULL);
+	map[0] = "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1";
+	map[1] = "1 0 0 1 0 0 0 0 1 0 0 0 1 0 1 1 0 1 0 1";	
+	map[2] = "1 0 0 1 0 1 1 0 1 0 1 1 1 0 1 1 0 1 0 1";
+	map[3] = "1 0 0 1 0 1 1 0 1 0 1 0 0 0 1 0 0 0 0 1";
+	map[4] = "1 0 0 0 0 0 0 0 1 0 0 0 1 0 0 0 1 1 0 1";
+	map[5] = "1 1 1 1 1 1 0 0 0 0 1 1 1 0 1 0 1 1 0 1";
+	map[6] = "1 0 0 0 1 0 0 1 1 0 1 0 1 1 1 0 0 0 1 1";
+	map[7] = "1 0 1 0 1 0 1 0 0 0 1 1 0 0 0 0 1 0 0 1";
+	map[8] = "1 0 0 0 1 0 1 0 1 0 0 0 0 1 1 1 1 1 0 1";
+	map[9] = "1 1 0 1 1 0 1 1 0 0 1 1 1 1 1 0 1 0 0 1";
+	map[10] = "1 0 0 1 1 0 1 1 0 0 1 1 1 1 1 0 1 0 0 1";
+	map[11] = "1 1 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 1";
+	map[12] = "1 0 0 0 1 1 0 0 1 1 1 1 1 1 0 1 0 1 0 1";
+	map[13] = "1 0 1 0 1 0 0 0 0 1 0 0 0 0 0 1 1 1 1 1";
+	map[14] = "1 1 0 0 0 0 0 1 1 0 0 1 0 1 1 1 1 1 0 1";
+	map[15] = "1 0 0 0 1 0 1 0 1 0 1 0 0 0 1 0 0 0 0 1";
+	map[16] = "1 0 1 0 1 0 1 0 1 0 1 0 0 0 1 0 1 1 1 1";
+	map[17] = "1 0 1 0 1 1 1 0 0 0 1 0 1 0 0 0 1 0 0 1";
+	map[18] = "1 0 1 0 0 0 0 0 1 0 0 0 1 0 0 0 0 0 0 1";
+	map[19] = "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1";
+	return (map);
 }
 
 int		parsing(char *filename, t_mlx *mlx)
 {
-	int		fd;
-	char	*buffer;
+	//int		fd;
+	//char	*buffer;
 	char	**tab;
-	int		k;
+	//int		k;
 
-	k = -1;
-	fd = open(filename, O_RDONLY, S_IREAD);
-	Y_NBR = 0;
-	while (get_next_line(fd, &buffer) > 0)
+	//k = -1;
+	//fd = open(filename, O_RDONLY, S_IREAD);*/
+	Y_NBR = 20;
+	ft_putstr(filename);
+	printf("\n");
+	/*while (get_next_line(fd, &buffer) > 0)
 	{
 		Y_NBR++;
 		free(buffer);
 	}
 	free(buffer);
-	close(fd);
-	tab = fill_tab(Y_NBR, filename);
-	GRID = parse_in_int(tab, Y_NBR);
-	while (++k < Y_NBR)
-		free(tab[k]);
+	close(fd);*/
+	//tab = fill_tab(Y_NBR, filename);
+	if ((tab = create_map(Y_NBR)) == NULL)
+		return (-1);
+	if ((GRID = parse_in_int(tab, Y_NBR)) == NULL)
+		return (-1);
+	check_grid(mlx);
+	/*while (++k < Y_NBR)
+		free(tab[k]);*/
 	free(tab);
+	//tab = free_tab(tab);
 	return (0);
 }
